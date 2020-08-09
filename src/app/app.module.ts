1280953c1c2f6fe8a697e1771f733527030e400f
kamilbieniasz <kamilbieniasz98@gmail.com>
2020-08-09 11:28:46 +0200

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChatContentComponent } from './chat-content/chat-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
